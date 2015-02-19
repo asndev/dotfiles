7cba9a0b367189a40c66076a78c78e796a1c2491
stepan <stepan@ws-asn.patronas.int>
2015-02-19 13:58:07 +0100

    init

---
+++ javascript/jshint.conf.js
@@ -0,0 +1,29 @@
+{
+  // Options
+  "camelcase": true,
+  "curly": true,
+  "eqeqeq": true,
+  "freeze": true,
+  "indent": 2,
+  "latedef": true,
+  "newcap": true,
+  "noarg": true,
+  "noempty": true,
+  "nonew": true,
+  "onevar": true,
+  "plusplus": false,
+  "quotmark": true,
+  "undef": true,
+  "unused": true,
+  "strict": true,
+  "maxparams": 10,
+  "maxdepth": 3,
+  "maxlen": 120,
+
+  // Globals
+  "globals": {
+    "browser": true,
+    "window": true,
+    "document": true
+  }
+}
---
+++ sublime/README.md
@@ -0,0 +1,9 @@
+Sublime Settings
+
+linux default:
+~/.config/sublime-text-3/Packages/
+~/.config/sublime-text-3/Installed\ Packages/
+
+mac default:
+~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/
